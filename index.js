d552bfbdf46fd00e5b01872c937ba58d9f2eec96
jonorl <39122230+jonorl@users.noreply.github.com>
2025-02-26 10:19:27 +0300

    Feat: Added comments

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const express = require("express");
 const app = express();
 const PORT = 3001;
 
+// This is to make index work when there's no paths
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+
+// Dynamically search if a html file matching the path name exists, if not send 404.html
 app.get("/:URLpath", (req, res) => {
   let URLpath = req.url.substring(1) + ".html";
 
