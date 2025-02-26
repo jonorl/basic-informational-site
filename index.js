602fc0cfdf284ff117ac06a5535f5d6aef958dfa
jonorl <39122230+jonorl@users.noreply.github.com>
2025-02-26 10:18:25 +0300

    Modified this to make it work with Express

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-let http = require("http");
-let url = require("url");
-let fs = require("fs/promises");
+let fs = require("fs");
+const path = require("path");
+const express = require("express");
+const app = express();
+const PORT = 3001;
 
-http
-  .createServer(async function (req, res) {
-    let q = url.parse(req.url, true);
-    if (q.pathname === "/") {
-      filename = "./index.html";
-    } else {
-      filename = "." + q.pathname + ".html";
-    }
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-    try {
-      const fileData = await fs.readFile(filename);
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(fileData);
-      return res.end();
-    } catch (err) {
-      const errorPage = await fs.readFile("./404.html");
-      res.writeHead(404, { "Content-Type": "text/html" });
-      res.write(errorPage);
-      return res.end();
-    }
-  })
-  .listen(8080);
+app.get("/:URLpath", (req, res) => {
+  let URLpath = req.url.substring(1) + ".html";
+
+  try {
+    fs.statSync(URLpath).isFile();
+    res.sendFile(path.join(__dirname, URLpath));
+  } catch (err) {
+    res.sendFile(path.join(__dirname, "404.html"));
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
